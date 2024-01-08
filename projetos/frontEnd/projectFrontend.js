72ac860645d887537673f2c960dae5b01450712c
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-01-08 20:38:44 -0300

    fazendo a pagina nishiconsult

--- projetos/frontEnd/projectFrontend.js
+++ projetos/frontEnd/projectFrontend.js
@@ -1,3 +1,8 @@
 let cardMa = document.querySelector("#cardMaConsultoriaHospitalar");
 
 cardMa.addEventListener("click",()=>{window.open("https://maconsultoriahospitalar.com.br","_blank")})
+
+let cardNishi = document.querySelector("#cardMaNishiConsult");
+
+
+cardNishi.addEventListener("click",()=>{window.open("./projetoNishiconsult.html","_blank")})
