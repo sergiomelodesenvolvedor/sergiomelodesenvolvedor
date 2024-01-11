f44760ac55ec57bd7d6eb807c2e41d8b3b7496e7
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-01-10 21:01:15 -0300

    finalizando o a função de abrir fechar menu da pagina projetos em frontend

--- projetos/frontEnd/projectFrontend.js
+++ projetos/frontEnd/projectFrontend.js
@@ -10,15 +10,53 @@ cardNishi.addEventListener("click", () => {
   window.open("./projetoNishiconsult.html", "_blank");
 });
 
+let xEstado = "menu";
+
+//aqui vou fazer a parte de abrir o x do menu
+
 let divMenuHrs = document.querySelector("#divMenuHrs");
 
-divMenuHrs.addEventListener("mouseover", () => {
-  document.querySelector("#menuComOptions").style.height = "40vh";
-  document.querySelector("#menuComOptions").style.opacity = "1";
-  document.querySelector("#hrMenuLinha1").style.width="1.9vw";
-  document.querySelector("#hrMenuLinha2").style.width="1.9vw";  
-  document.querySelector("#hrMenuLinha1").style.transform = "rotate(-44deg)"
-  document.querySelector("#hrMenuLinha2").style.transform = "rotate(44deg)"
-  document.querySelector("#hrMenuLinha2").style.top = "-1.6vh"
+divMenuHrs.addEventListener("click", async () => {
+  if (xEstado === "menu") {
+    document.querySelector("#menuComOptions").style.height = "40vh";
+    document.querySelector("#menuComOptions").style.opacity = "1";
+    document.querySelector("#hrMenuLinha1").style.width = "1.9vw";
+    document.querySelector("#hrMenuLinha2").style.width = "1.9vw";
+    document.querySelector("#hrMenuLinha1").style.transform = "rotate(-44deg)";
+    document.querySelector("#hrMenuLinha2").style.transform = "rotate(44deg)";
+    document.querySelector("#hrMenuLinha2").style.top = "-1.6vh";
 
+    xEstado = "x";
+  } else {
+    menuComOptions.style.opacity = "0";
+    menuComOptions.style.height = "0px";
+    document.querySelector("#hrMenuLinha1").style.transform = "rotate(0deg)";
+    document.querySelector("#hrMenuLinha2").style.transform = "rotate(0deg)";
+    document.querySelector("#hrMenuLinha2").style.top = "0vh";
+    document.querySelector("#hrMenuLinha1").style.width = "4vw";
+    document.querySelector("#hrMenuLinha2").style.width = "3vw";
+    xEstado = "menu";
+  }
 });
+
+let menuComOptions = document.querySelector("#menuComOptions");
+
+document.addEventListener("click", (event) => {
+  if (
+    !menuComOptions.contains(event.target) &&
+    !divMenuHrs.contains(event.target) &&
+    xEstado === "x"
+  ) {
+    menuComOptions.style.opacity = "0";
+    menuComOptions.style.height = "0px";
+
+    document.querySelector("#hrMenuLinha1").style.transform = "rotate(0deg)";
+    document.querySelector("#hrMenuLinha2").style.transform = "rotate(0deg)";
+    document.querySelector("#hrMenuLinha2").style.top = "0vh";
+    document.querySelector("#hrMenuLinha1").style.width = "4vw";
+    document.querySelector("#hrMenuLinha2").style.width = "3vw";
+    xEstado = "menu";
+  }
+});
+
+//aqui vou fazer a parte de fechar o x do menu
