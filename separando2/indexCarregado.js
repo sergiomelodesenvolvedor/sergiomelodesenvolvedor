f72b31fe0bba77efd0e91ef46027ce56c8291aa4
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-01-28 21:09:24 -0300

    t

--- separando2/indexCarregado.js
+++ separando2/indexCarregado.js
@@ -1,4 +1,14 @@
-let letraSfundo = document.querySelector("#svg-letra-s");
+let tudoDepoisDeCarregado = document.querySelector("#tudoDepoisDeCarregado")
+
+window.addEventListener('load',()=>{
+    setTimeout(()=>{
+
+    
+    tudoDepoisDeCarregado.style.display="block"
+},9000)
+
+    setTimeout(()=>{tudoDepoisDeCarregado.style.opacity="1"},9100)
+})
 
 
 
