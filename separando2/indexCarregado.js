16be7fe6a3fdc3360ab983e8c013051b1aacba8e
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-01-29 21:49:07 -0300

    t

--- separando2/indexCarregado.js
+++ separando2/indexCarregado.js
@@ -1,12 +1,12 @@
-let containerParte1 = document.querySelector("#container-parte-1");
+let divTodasAsPartes = document.querySelector("#div-todas-as-partes");
 
 window.addEventListener("load", () => {
   setTimeout(() => {
-    containerParte1.style.display = "block";
-  }, 0 /* 9000 */);
+    divTodasAsPartes.style.display = "block";
+  }, 9000 /* 9000 */);
 
   setTimeout(() => {
-    containerParte1.style.opacity = "1";
+    divTodasAsPartes.style.opacity = "1";
     // Configuração do Typed.js
     var options = {
       strings: ["", "Desenvolvedor Web <br/> Full Stack"],
@@ -18,7 +18,7 @@ window.addEventListener("load", () => {
 
     // Inicializa o Typed.js no elemento com o ID "typed-output"
     var typed = new Typed("#h1-desenvolvedor-web-full-stack", options);
-  }, 0 /* 9100 */);
+  }, 9100 /* 9100 */);
 });
 
 let line1 = document.querySelector("#line1");
@@ -26,14 +26,13 @@ let svgCanto1 = document.querySelector("#svg-canto-1");
 let circleFora1 = document.querySelector("#circle-fora-1");
 let circleDentro1 = document.querySelector("#circle-dentro-1");
 let colTudoMinhaHistoria = document.querySelector("#col-tudo-minha-historia");
-
+console.log(colTudoMinhaHistoria.offsetHeight)
 //aqui estou ajustando a primeira linha do canto para redimensionar de acordo com o tamanho da tela
 line1.setAttribute("y2", "1000vh");
 circleFora1.setAttribute("cy", colTudoMinhaHistoria.offsetHeight / 2);
 circleDentro1.setAttribute("cy", colTudoMinhaHistoria.offsetHeight / 2);
 svgCanto1.style.height = colTudoMinhaHistoria.offsetHeight;
 
-let divTodasAsPartes = document.querySelector("#div-todas-as-partes");
 let divQueSeEsconde = document.querySelector("#div-que-se-esconde");
 let posicaoPagina = 0;
 
@@ -83,7 +82,3 @@ if (window.innerWidth >= 992 && window.innerWidth <=1200) {
     });
   }
   
-/* 
-h1MinhaHistoria.style.border = "1px solid black";
-
-containerParte1.style.border = "1px solid black"; */
