c0cdcbbb8f90be7475554aab478ae8749b685bd4
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-01-29 22:08:36 -0300

    t

--- separando2/indexCarregado.js
+++ separando2/indexCarregado.js
@@ -26,59 +26,59 @@ let svgCanto1 = document.querySelector("#svg-canto-1");
 let circleFora1 = document.querySelector("#circle-fora-1");
 let circleDentro1 = document.querySelector("#circle-dentro-1");
 let colTudoMinhaHistoria = document.querySelector("#col-tudo-minha-historia");
-console.log(colTudoMinhaHistoria.offsetHeight)
 //aqui estou ajustando a primeira linha do canto para redimensionar de acordo com o tamanho da tela
-line1.setAttribute("y2", "1000vh");
-circleFora1.setAttribute("cy", colTudoMinhaHistoria.offsetHeight / 2);
-circleDentro1.setAttribute("cy", colTudoMinhaHistoria.offsetHeight / 2);
-svgCanto1.style.height = colTudoMinhaHistoria.offsetHeight;
+setTimeout(() => {
+  line1.setAttribute("y2", "1000vh");
+  circleFora1.setAttribute("cy", colTudoMinhaHistoria.offsetHeight / 2);
+  circleDentro1.setAttribute("cy", colTudoMinhaHistoria.offsetHeight / 2);
+
+  svgCanto1.style.height = colTudoMinhaHistoria.offsetHeight;
+}, 9100);
 
 let divQueSeEsconde = document.querySelector("#div-que-se-esconde");
 let posicaoPagina = 0;
 
 let ultimoTempoDoWhell = 0;
-if (window.innerWidth >= 992 && window.innerWidth <=1200) {
-    divTodasAsPartes.addEventListener("wheel", (event) => {
-      const agora = new Date().getTime();
-  
-      if (agora - ultimoTempoDoWhell > 500) {
-        if (event.deltaY > 0) {
-          if (posicaoPagina !== -234) {
-            posicaoPagina -= 117;
-            divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
-          }
-        }
-        if (event.deltaY < 0) {
-          if (posicaoPagina !== 0) {
-            posicaoPagina += 117;
-            divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
-          }
-        }
-        ultimoTempoDoWhell=agora
-      }
-    });
-  }
+if (window.innerWidth >= 992 && window.innerWidth <= 1200) {
+  divTodasAsPartes.addEventListener("wheel", (event) => {
+    const agora = new Date().getTime();
 
-  
-  if (window.innerWidth >= 1200) {
-    divTodasAsPartes.addEventListener("wheel", (event) => {
-      const agora = new Date().getTime();
-  
-      if (agora - ultimoTempoDoWhell > 500) {
-        if (event.deltaY > 0) {
-          if (posicaoPagina !== -210) {
-            posicaoPagina -= 105;
-            divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
-          }
+    if (agora - ultimoTempoDoWhell > 500) {
+      if (event.deltaY > 0) {
+        if (posicaoPagina !== -234) {
+          posicaoPagina -= 117;
+          divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
         }
-        if (event.deltaY < 0) {
-          if (posicaoPagina !== 0) {
-            posicaoPagina += 105;
-            divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
-          }
-        }
-        ultimoTempoDoWhell=agora
       }
-    });
-  }
-  
+      if (event.deltaY < 0) {
+        if (posicaoPagina !== 0) {
+          posicaoPagina += 117;
+          divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
+        }
+      }
+      ultimoTempoDoWhell = agora;
+    }
+  });
+}
+
+if (window.innerWidth >= 1200) {
+  divTodasAsPartes.addEventListener("wheel", (event) => {
+    const agora = new Date().getTime();
+
+    if (agora - ultimoTempoDoWhell > 500) {
+      if (event.deltaY > 0) {
+        if (posicaoPagina !== -210) {
+          posicaoPagina -= 105;
+          divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
+        }
+      }
+      if (event.deltaY < 0) {
+        if (posicaoPagina !== 0) {
+          posicaoPagina += 105;
+          divQueSeEsconde.style.transform = `translateY(${posicaoPagina}vh)`;
+        }
+      }
+      ultimoTempoDoWhell = agora;
+    }
+  });
+}
