5bd86e366a372650d936fd6a3e613240679b9a39
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-02-19 19:50:56 -0300

    t

--- separando2/projetos/projetosFrontEnd/projetosFrontend.js
+++ separando2/projetos/projetosFrontEnd/projetosFrontend.js
@@ -89,4 +89,26 @@ let subirDescerLetraS = async () => {
     subirDescerLetraM();
     subirDescerLetraD();
     
+  
+
+    //aqui vou fazer a parte de quando clicar no menu hamburguer
+
+    let menu = document.querySelector("#menu")
     
+
+    let menuHamburguer = document.querySelector("#menu-hamburger")
+    
+    menuHamburguer.addEventListener('click',()=>{
+    menu.style.opacity="1"
+    menuHamburguer.style.opacity="0";
+    menuHamburguer.style.display="none";  
+    })
+
+    let iconFecharMenu = document.querySelector("#icon-fechar-menu")
+    iconFecharMenu.addEventListener('click',()=>{
+      
+      menu.style.opacity="0"
+      menuHamburguer.style.display="block"
+      menuHamburguer.style.opacity="1"
+
+    })
