d5411b1043159cfdeb6bab127c93a8854304906f
sergiomelodesenvolvedor <sergiomelo.desenvolvedor@gmail.com>
2024-02-19 21:15:07 -0300

    t

--- separando2/projetos/projetosBackEnd/projetosBackend.js
+++ separando2/projetos/projetosBackEnd/projetosBackend.js
@@ -1,1 +1,32 @@
-alert(window.innerWidth+'x'+window.innerHeight)
+//aqui vou fazer a parte de quando clicar no menu hamburguer
+
+let menu = document.querySelector("#menu")
+    
+
+let menuHamburguer = document.querySelector("#menu-hamburger")
+
+menuHamburguer.addEventListener('click',()=>{
+menu.style.opacity="1"
+menuHamburguer.style.opacity="0";
+menuHamburguer.style.display="none";  
+})
+
+let iconFecharMenu = document.querySelector("#icon-fechar-menu")
+iconFecharMenu.addEventListener('click',()=>{
+  
+  menu.style.opacity="0"
+  menuHamburguer.style.display="block"
+  menuHamburguer.style.opacity="1"
+
+})
+
+document.addEventListener("click",(e)=>{
+  let clicouFora = !menu.contains(e.target) && !menuHamburguer.contains(e.target);
+  if(clicouFora){
+    menu.style.opacity="0"
+    menuHamburguer.style.display="block"
+    menuHamburguer.style.opacity="1"
+    
+  }
+  
+})
